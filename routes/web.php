75a29c4fcc73394507808abeaa32212d3ad61c27
Danil <46190654+GrenkiLoveDeveloper@users.noreply.github.com>
2024-02-16 21:16:58 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Health check passed';
+    // return view('welcome');
 });
 
 Route::get('/h', function () {
