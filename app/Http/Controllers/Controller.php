fa09cbd542dc0a5ca768c7087c613739518a3282
Danil <46190654+GrenkiLoveDeveloper@users.noreply.github.com>
2024-04-15 21:44:54 +0300

    test:test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests;
+    use ValidatesRequests;
+
+    public function __construct()
+    {
+        Log::info('This is an info log');
+        Log::error('This is an error log');
+    }
 }
